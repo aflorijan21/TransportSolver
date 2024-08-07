9aeb75cb3f9a9c4a0dda03f215cc5645f3f3663e
aflorijan21 <aflorijan21@foi.hr>
2024-08-07 19:22:16 +0200

    Napravljen dizajn forme.

--- TransportSolver/TransportSolver/Form1.Designer.cs
+++ TransportSolver/TransportSolver/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TransportSolver
 {
-    partial class Form1
+    partial class FrmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,149 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.cmbMethod = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvMatrix = new System.Windows.Forms.DataGridView();
+            this.nudRows = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nudColumns = new System.Windows.Forms.NumericUpDown();
+            this.lblMatrixSize = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatrix)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudRows)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudColumns)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // cmbMethod
+            // 
+            this.cmbMethod.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMethod.FormattingEnabled = true;
+            this.cmbMethod.Location = new System.Drawing.Point(112, 12);
+            this.cmbMethod.Name = "cmbMethod";
+            this.cmbMethod.Size = new System.Drawing.Size(198, 21);
+            this.cmbMethod.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Odaberite metodu:";
+            // 
+            // dgvMatrix
+            // 
+            this.dgvMatrix.AllowUserToAddRows = false;
+            this.dgvMatrix.AllowUserToDeleteRows = false;
+            this.dgvMatrix.AllowUserToResizeColumns = false;
+            this.dgvMatrix.AllowUserToResizeRows = false;
+            this.dgvMatrix.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMatrix.ColumnHeadersVisible = false;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvMatrix.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvMatrix.Location = new System.Drawing.Point(203, 75);
+            this.dgvMatrix.Name = "dgvMatrix";
+            this.dgvMatrix.RowHeadersVisible = false;
+            this.dgvMatrix.Size = new System.Drawing.Size(504, 463);
+            this.dgvMatrix.TabIndex = 2;
+            // 
+            // nudRows
+            // 
+            this.nudRows.Location = new System.Drawing.Point(271, 545);
+            this.nudRows.Name = "nudRows";
+            this.nudRows.Size = new System.Drawing.Size(47, 20);
+            this.nudRows.TabIndex = 3;
+            this.nudRows.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.nudRows.ValueChanged += new System.EventHandler(this.nudRows_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(590, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Broj stupaca:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(205, 547);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Broj redova:";
+            // 
+            // nudColumns
+            // 
+            this.nudColumns.Location = new System.Drawing.Point(660, 48);
+            this.nudColumns.Name = "nudColumns";
+            this.nudColumns.Size = new System.Drawing.Size(47, 20);
+            this.nudColumns.TabIndex = 6;
+            this.nudColumns.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.nudColumns.ValueChanged += new System.EventHandler(this.nudColumns_ValueChanged);
+            // 
+            // lblMatrixSize
+            // 
+            this.lblMatrixSize.AutoSize = true;
+            this.lblMatrixSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMatrixSize.Location = new System.Drawing.Point(427, 32);
+            this.lblMatrixSize.Name = "lblMatrixSize";
+            this.lblMatrixSize.Size = new System.Drawing.Size(60, 31);
+            this.lblMatrixSize.TabIndex = 7;
+            this.lblMatrixSize.Text = "3x3";
+            // 
+            // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(834, 429);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(923, 580);
+            this.Controls.Add(this.lblMatrixSize);
+            this.Controls.Add(this.nudColumns);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nudRows);
+            this.Controls.Add(this.dgvMatrix);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbMethod);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Transport Solver";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatrix)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudRows)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudColumns)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cmbMethod;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvMatrix;
+        private System.Windows.Forms.NumericUpDown nudRows;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nudColumns;
+        private System.Windows.Forms.Label lblMatrixSize;
     }
 }
 
